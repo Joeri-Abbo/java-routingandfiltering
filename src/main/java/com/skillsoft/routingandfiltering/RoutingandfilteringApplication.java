b699b311a6c95ef01b84ebb584d36eff9596640d
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-11 14:41:03 +0200

    Iniut

---
+++ src/main/java/com/skillsoft/routingandfiltering/RoutingandfilteringApplication.java
@@ -0,0 +1,13 @@
+package com.skillsoft.routingandfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingandfilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingandfilteringApplication.class, args);
+	}
+
+}
